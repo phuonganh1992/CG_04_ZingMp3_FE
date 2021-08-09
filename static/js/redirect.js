2e4b0033c5ab8d9ab0b982aaf98f991c078b114b
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-08-09 11:54:36 +0700

    .

--- static/js/redirect.js
+++ static/js/redirect.js
@@ -1,22 +1,26 @@
-// function redirectView(){
-//     window.location.href="user_view.html";
-// }
-// function redirectListUser(){
-//     window.location.href="user_list.html";
-// }
-//
-// function redirectHome(){
-//     window.location.href="zing_layout.html"
-// }
 function redirectView(){
-    window.location.href="http://localhost:63343/ZingMp3_FE/templates/user_view.html";
+    window.location.href="user_view.html";
 }
 function redirectListUser(){
-    window.location.href="http://localhost:63343/ZingMp3_FE/templates/user_list.html";
+    window.location.href="user_list.html";
 }
-function redirectPlaylist(){
-    window.location.href="http://localhost:63343/ZingMp3_FE/templates/playlist_list.html"
-}
+
 function redirectHome(){
-    window.location.href="http://localhost:63343/ZingMp3_FE/templates/zing_home.html"
+    window.location.href="zing_layout.html"
 }
+
+function redirectPlaylist(){
+    window.location.href="playlist_list.html"
+}
+// function redirectView(){
+//     window.location.href="http://localhost:63343/ZingMp3_FE/templates/user_view.html";
+// }
+// function redirectListUser(){
+//     window.location.href="http://localhost:63343/ZingMp3_FE/templates/user_list.html";
+// }
+// function redirectPlaylist(){
+//     window.location.href="http://localhost:63343/ZingMp3_FE/templates/playlist_list.html"
+// }
+// function redirectHome(){
+//     window.location.href="http://localhost:63343/ZingMp3_FE/templates/zing_home.html"
+// }
